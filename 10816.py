4263e7d1540860a8045c86d83de9cc2064410eba
jension <a24349663@gmail.com>
2022-11-06 02:36:46 +0900

    study

--- 10816.py
+++ 10816.py
@@ -6,10 +6,18 @@
 # 접근 방법 : 문제에서 주어진 입력의 개수는 500,000이 넘음 -> O(N)으로 풀면 안될 것 같음, 그리고 밑에 '이분탐색'키워드를 봐버림 정렬알고리즘 이용하자
 
 #경계값 찾는 알고리즘 연습하기(이진탐색 이용)
-A_list = [1, 1, 2, 2, 3, 4, 4, 4, 6]
+
+
+import sys
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
 
 # 1. 기본의 이분탐색 알고리즘   O(logN)
 def BSearch(start, end, array, target):
+    if(start > end):
+        return None
+    
     mid = (start + end)//2
     if(array[mid] == target):
         return mid
@@ -17,15 +25,17 @@ def BSearch(start, end, array, target):
     elif array[mid] > target:
         return BSearch(start, mid-1, array, target)
     #찾으려는 값이 mid 보다 오른쪽에 있음
-    elif array[mid] < target:
-         return BSearch(mid+1,end, array, target)
-    else:
-        return False
+    else: 
+        array[mid] < target
+        return BSearch(mid+1,end, array, target)
 
 # 2. Lowwer Bound 알고리즘
 # 아이디어 : 이분탐색으로 정렬 후 나오는 target의 인덱스 기준으로, 왼쪽으로 줄였을 때 target값이 달라질 때 까지 
 #          반복해서 return해야할 값을 -1 해서 시작 경계값을 return한다.
 def BSearch_left(start, end, array, target):
+    #Base case
+    if(start > end):
+        return 0
     mid = (start + end) // 2
     # 값 일단 찾음
     if(array[mid] == target):
@@ -35,13 +45,15 @@ def BSearch_left(start, end, array, target):
         return mid+1
     elif array[mid] > target:
         return BSearch_left(start, mid-1, array, target)
-    elif array[mid] < target:
-        return BSearch_left(mid+1, end, array, target)
     else:
-        return 0
+        array[mid] < target
+        return BSearch_left(mid+1, end, array, target)
+    
     
     
 def BSearch_right(start, end, array, target):
+    if(start > end):
+        return 0
     mid = (start + end) // 2
     # 값 일단 찾음
     if(array[mid] == target):
@@ -51,15 +63,28 @@ def BSearch_right(start, end, array, target):
         return mid 
     elif array[mid] > target:
         return BSearch_right(start, mid-1, array, target)
-    elif array[mid] < target:
+    else: 
+        array[mid] < target
         return BSearch_right(mid+1, end, array, target)
-    else:
-        return 0
+    
 
+def BSearch_left2(start, end, array, target):
+    while start<=end:
+        mid = (start + end) // 2
+        if(array[mid] < target):
+            start = mid +1
+        else:
+            end = mid - 1
+    return start
 
-print(BSearch_left(0, len(A_list)-1, A_list, 1))   #Lower bound
-print(BSearch_right(0, len(A_list)-1, A_list, 1))   #Upper bound
-
+def BSearch_right2(start, end, array, target):
+    while start <=end:
+        mid = (start + end) // 2
+        if(array[mid] <= target):
+            start = mid +1
+        else:
+            end = mid -1
+    return start
 
 #문제점 : target을 6으로 했을 때 index out of range 발생. -> 49번째줄 while문 안에 조건 추가, 33번째 줄에도 동일하게 추가해줌
 
@@ -70,8 +95,12 @@ M = int(input())
 M_list = list(map(int, input().split()))
 
 #출력할 빈 리스트 생성
-result = []
+
 # N 정렬
 N_list.sort()
 
-print(BSearch(0, len(N_list)-1), N_list, 10)
+
+
+for i in M_list:
+    print(BSearch_right2(0, len(N_list)-1, N_list, i)- BSearch_left2(0, len(N_list)-1, N_list, i), end = ' ')
+
