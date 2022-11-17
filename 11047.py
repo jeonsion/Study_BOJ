5df26330f14cdfead98a4a5b2b0c8546dcb78b5a
jension <a24349663@gmail.com>
2022-11-17 13:34:49 +0900

    study

---
+++ 11047.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+count = 0
+cash_list = [int(input()) for i in range(N)]
+cash_list.reverse()
+
+#그리디 알고리즘 : 각 케이스에 최선의 경우
+i = 0
+while(K != 0 ):
+    if K - cash_list[i] >= 0:
+        K = K-cash_list[i]
+        count+=1
+    else:
+        i+=1
+print(count)
